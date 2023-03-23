1c4fd2e487f580cf023fd7df9c58c1df4eec1333
kaisle-sky <kyle.sky19@gmail.com>
2023-03-23 10:50:37 -0500

    created script to read text file and count the number of words in the file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+
+def count_words(text):
+    words = text.split()
+    print(len(words))
+
+
+with open("./books/frankenstein.txt") as f:
+    text_read = f.read()
+
+count_words(text_read)
